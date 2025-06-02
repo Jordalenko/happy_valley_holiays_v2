934df8a0e18657fb80cab5b52ce3b32a42f6ab2e
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-06-03 00:25:55 +0100

    Fix remaining misspellings

--- happy_valley_Holidays/asgi.py
+++ happy_valley_Holidays/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'happy_valley_Holidays.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'happy_valley_holidays.settings')
 
 application = get_asgi_application()
--- happy_valley_Holidays/settings.py
+++ happy_valley_Holidays/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'happy_valley_Holidays.urls'
+ROOT_URLCONF = 'happy_valley_holidays.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'happy_valley_Holidays.wsgi.application'
+WSGI_APPLICATION = 'happy_valley_holidays.wsgi.application'
 
 
 # Database
