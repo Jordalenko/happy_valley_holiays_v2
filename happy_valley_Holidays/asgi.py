2e9452124b105dba32d9fbfa4d011466b949e761
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-06-02 23:28:29 +0100

    initial commit

---
+++ happy_valley_Holidays/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for happy_valley_Holidays project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'happy_valley_Holidays.settings')
+
+application = get_asgi_application()
