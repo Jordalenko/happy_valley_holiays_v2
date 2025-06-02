a3d21b9fe54c878891dcd03a74af49e30bc7d025
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-06-03 00:22:37 +0100

    fix manage.py

--- happy_valley_Holidays/wsgi.py
+++ happy_valley_Holidays/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for happy_valley_Holidays project.
+WSGI config for happy_valley_holidays project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
