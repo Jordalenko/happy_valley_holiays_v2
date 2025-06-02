c88253bac458bf40d99482d2468e996369b08d9f
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-06-02 23:41:50 +0100

    deploy to heroku

--- happy_valley_Holidays/settings.py
+++ happy_valley_Holidays/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--k=ief#pat$-7%&w5l4_q)^!qt#3yn5kog#a$b2aqb_zvv1+%9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com'
+    '127.0.0.0'
+    '127.0.0.1'
+]
 
 
 # Application definition
