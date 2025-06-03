bf18e2c3a5f4b6ba24eae26c5307a5b0de1674bd
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-06-03 03:33:11 +0100

    Add cottages app template

--- cottages/views.py
+++ cottages/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Cottage, Review
 
 # Create your views here.
+class CottageList(generic.ListView):
+    queryset = Cottage.objects.all()
+    template_name = "cottage_list.html"
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the cottages index.")
+class ReviewList(generic.ListView):
+    queryset = Review.objects.all()
+    template_name = "cottage_list.html"
--- happy_valley_holidays/urls.py
+++ happy_valley_holidays/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from cottages import views as index_views
+# from cottages import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path("", include("cottages.urls"), name="cottages-urls"),,
     path('admin/', admin.site.urls),
 ]
