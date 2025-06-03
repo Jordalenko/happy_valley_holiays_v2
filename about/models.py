9bd4e8e2758913aec0828500c33c5c26a0b0b35d
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-06-03 22:33:11 +0100

    Add code to about app to facilitate image server.

--- about/models.py
+++ about/models.py
@@ -7,7 +7,7 @@ from cloudinary.models import CloudinaryField
 class About(models.Model):
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
-    featured_image = CloudinaryField('image', default='placeholder')
+    profile_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
 
     def __str__(self):
