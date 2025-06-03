fea04f99eb15f7d6d83ecd45040a4cd8cd42a1dd
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-06-03 16:49:28 +0100

    added authentication

--- about/views.py
+++ about/views.py
@@ -4,7 +4,7 @@ from .models import About
 
 def about_me(request):
     """
-    Renders the About page
+    Renders the most recent About page
     """
     about = About.objects.all().order_by('-updated_on').first()
 
--- happy_valley_holidays/settings.py
+++ happy_valley_holidays/settings.py
@@ -47,12 +47,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cottages',
     'guest_profile',
     'bookings',
     'about',
     'django_summernote',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -63,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'happy_valley_holidays.urls'
@@ -123,6 +132,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
 # Internationalization
