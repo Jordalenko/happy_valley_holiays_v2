8cd0acffa37182270f7fa3b63c1c81f450366e16
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-06-12 17:40:26 +0100

    Add password change functionality for logged in users.

--- happy_valley_holidays/urls.py
+++ happy_valley_holidays/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -25,6 +26,13 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('password_change/', 
+         auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), 
+         name='password_change'),
+
+    path('password_change/done/', 
+         auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
+         name='password_change_done'),
     path("", include("cottages.urls"), name="cottages-urls"),
 ]
 
